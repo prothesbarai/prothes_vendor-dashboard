97b98db5c4e8bdcb711a9c58c2139ebc422a82a8
Prothes Barai <75435992+prothesbarai@users.noreply.github.com>
2025-10-15 16:16:39 +0600

    Update app_colors.dart

--- lib/utils/constant/app_colors.dart
+++ lib/utils/constant/app_colors.dart
@@ -9,4 +9,7 @@ class AppColors {
 
   static const Color appInputFieldActiveColor = Color(0x66BF31BF);
   static const Color appInputFieldUnActiveColor = Color(0x7e2f2f31);
+
+  static const Color dangerColor = Color(0xFFDC3545);
+  static const Color success = Color(0xFF21b531);
 }
