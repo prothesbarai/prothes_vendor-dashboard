d0fae174c8bac5691cc6da4782ec9001c94774ce
prothesbarai <aspprothesbarai1619@gmail.com>
2025-10-24 11:42:09 +0600

    asp

--- lib/service/hive_service.dart
+++ lib/service/hive_service.dart
@@ -1,9 +1,11 @@
-import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:path_provider/path_provider.dart';
 import '../models/hive_models/theme_selected_model/theme_selected_model.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
-class HiveService {
 
+/// >>> Only APP er Jonno
+/*class HiveService {
   static Future<void> initHive() async{
     var dir = await getApplicationDocumentsDirectory();
     Hive.init(dir.path);
@@ -12,7 +14,26 @@ class HiveService {
     await Future.wait([
       Hive.openBox('themeSettings'),
     ]);
-
   }
+}*/
 
+
+/// >>> Web / Chrome e Run Korar Jonno....
+class HiveService {
+  static Future<void> initHive() async {
+    if (kIsWeb) {
+      // Initialize Hive for Web
+      await Hive.initFlutter();
+    } else {
+      // Initialize Hive for Android/iOS/Desktop
+      final dir = await getApplicationDocumentsDirectory();
+      Hive.init(dir.path);
+    }
+
+    // Register your adapter
+    Hive.registerAdapter(AppThemeAdapter());
+
+    // Open boxes
+    await Hive.openBox('themeSettings');
+  }
 }
